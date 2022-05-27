d17a2b4c0ae53f9f3360619f0570057d043d3b3d
milicaMilovanovic <milicam@biosense.rs>
2022-05-27 17:22:48 +0200

    C:/Program Files/Git/

---
+++ src/main/java/com/bookstore/entity/Author.java
@@ -0,0 +1,81 @@
+package com.bookstore.entity;
+
+import java.io.Serializable;
+import javax.persistence.*;
+import java.util.List;
+
+
+/**
+ * The persistent class for the authors database table.
+ * 
+ */
+@Entity
+@Table(name="authors")
+@NamedQuery(name="Author.findAll", query="SELECT a FROM Author a")
+public class Author implements Serializable {
+	private static final long serialVersionUID = 1L;
+
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	private Integer id;
+
+	@Column(name="first_name")
+	private String firstName;
+
+	@Column(name="last_name")
+	private String lastName;
+
+	//bi-directional many-to-one association to Book
+	@OneToMany(mappedBy="author")
+	private List<Book> books;
+
+	public Author() {
+	}
+
+	public Integer getId() {
+		return this.id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getFirstName() {
+		return this.firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getLastName() {
+		return this.lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	public List<Book> getBooks() {
+		return this.books;
+	}
+
+	public void setBooks(List<Book> books) {
+		this.books = books;
+	}
+
+	public Book addBook(Book book) {
+		getBooks().add(book);
+		book.setAuthor(this);
+
+		return book;
+	}
+
+	public Book removeBook(Book book) {
+		getBooks().remove(book);
+		book.setAuthor(null);
+
+		return book;
+	}
+
+}
